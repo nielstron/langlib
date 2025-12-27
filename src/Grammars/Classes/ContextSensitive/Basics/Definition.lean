8a8c2356c19a9679091d93de108fa82034ea00b1
Niels <niels.muendler@inf.ethz.ch>
2025-12-27 13:33:49 +0100

    Fix cs def

--- src/Grammars/Classes/ContextSensitive/Basics/Definition.lean
+++ src/Grammars/Classes/ContextSensitive/Basics/Definition.lean
@@ -18,18 +18,18 @@ structure CS_grammar (T : Type) :=
 variables {T : Type}
 
 /-- One step of context-sensitive transformation. -/
-def CS_transforms (g : CS_grammar T) (w₁ w₂ : List (symbol T g.nt)) : Prop :=
-∃ r : csrule T g.nt, r ∈ g.rules  ∧  ∃ u v : List (symbol T g.nt), and
-  (w₁ = u ++ r.context_left ++ [symbol.nonterminal r.input_nonterminal] ++ r.context_right ++ v)
+def CS_transforms (g : CS_grammar T) (w₁ w₂ : List (symbol T g.nt))  : Prop :=
+  ∃r : csrule T g.nt, ∃u v : List (symbol T g.nt), r ∈ g.rules ∧
+  (w₁ = u ++ r.context_left ++ [symbol.nonterminal r.input_nonterminal] ++ r.context_right ++ v) ∧
   (w₂ = u ++ r.context_left ++ r.output_string ++ r.context_right ++ v)
 
 /-- Any number of steps of context-sensitive transformation; reflexive+transitive closure of `CS_transforms`. -/
 def CS_derives (g : CS_grammar T) : List (symbol T g.nt) → List (symbol T g.nt) → Prop :=
-Relation.refl_trans_gen (CS_transforms g)
+Relation.ReflTransGen (CS_transforms g)
 
 /-- The Set of words that can be derived from the initial nonterminal. -/
 def CS_language (g : CS_grammar T) : Language T :=
-λ w : List T, CS_derives g [symbol.nonterminal g.initial] (List.map symbol.terminal w)
+λ w : List T ↦ CS_derives g [symbol.nonterminal g.initial] (List.map symbol.terminal w)
 
 /-- Predicate "is context-sensitive"; defined by existence of a context-sensitive grammar for the given Language. -/
 def is_CS (L : Language T) : Prop :=
