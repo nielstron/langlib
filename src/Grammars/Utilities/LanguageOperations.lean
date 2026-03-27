939d82880c404ee8ca412476c4462223bc98e4ad
Niels <niels.muendler@inf.ethz.ch>
2026-03-27 10:04:47 +0100

    Fix reversal

--- src/Grammars/Utilities/LanguageOperations.lean
+++ src/Grammars/Utilities/LanguageOperations.lean
@@ -9,6 +9,12 @@ variable {T : Type _}
 def reverseLang (L : Language T) : Language T :=
   fun w : List T => w.reverse ∈ L
 
+@[simp] theorem reverseLang_reverseLang (L : Language T) :
+  reverseLang (reverseLang L) = L := by
+  funext w
+  change (w.reverse.reverse ∈ L) = (w ∈ L)
+  simp
+
 def bijemapLang {T' : Type _} (L : Language T) (π : T ≃ T') : Language T' :=
   fun w : List T' => List.map π.symm w ∈ L
 
