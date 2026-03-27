ed4e614754f6047a86e6ba3eab4c3f8895c01682
Niels <niels.muendler@inf.ethz.ch>
2026-03-27 11:02:17 +0100

    Add pumping lemma

---
+++ src/Grammars/Classes/ContextFree/PumpingCfg/Utils.lean
@@ -0,0 +1,47 @@
+import Mathlib.Data.List.Basic
+import Mathlib.Computability.ContextFreeGrammar
+
+variable {α : Type _}
+
+def nTimes (l : List α) (n : ℕ) : List α :=
+  (List.replicate n l).flatten
+
+infixl:69 " ^+^ " => nTimes
+
+variable {l : List α} {n : ℕ}
+
+lemma nTimes_succ_l : l^+^n.succ = l ++ l^+^n := by
+  simp [nTimes, List.replicate_succ]
+
+lemma nTimes_succ_r : l^+^n.succ = l^+^n ++ l := by
+  simp [nTimes, List.replicate_succ']
+
+lemma nTimes_map {β : Type _} {f : α → β} : (l.map f)^+^n = (l^+^n).map f := by
+  simp [nTimes]
+
+-- not used anywhere, just for fun
+lemma nTimes_add {m : ℕ} : l ^+^ (m + n) = l ^+^ m ++ l ^+^ n := by
+  induction n with
+  | zero => exact (l ^+^ m).append_nil.symm
+  | succ _ ih => rw [Nat.add_succ, nTimes_succ_r, nTimes_succ_r, ih, List.append_assoc]
+
+-- not used anywhere, just for fun
+lemma nTimes_mul {m : ℕ} : l ^+^ m * n = l ^+^ m ^+^ n := by
+  induction n with
+  | zero => rfl
+  | succ _ ih => rw [Nat.mul_succ, nTimes_add, ih, nTimes_succ_r]
+
+namespace ContextFreeGrammar
+
+universe uN uT
+
+variable {T : Type uT} {g : ContextFreeGrammar T}
+
+theorem Derives.head_induction_on {v : List (Symbol T g.NT)} {P : ∀ u, g.Derives u v → Prop}
+    {u : List (Symbol T g.NT)} (huv : g.Derives u v)
+    (refl : P v (Derives.refl v))
+    (head : ∀ {u w} (huw : g.Produces u w) (hwv : g.Derives w v), P w hwv → P u (hwv.head huw)) :
+    P u huv :=
+  Relation.ReflTransGen.head_induction_on huv refl head
+
+end ContextFreeGrammar
---
+++ src/Grammars/Classes/ContextFree/PumpingCfg/toMathlib.lean
@@ -0,0 +1,20 @@
+import Grammars.Classes.ContextFree.PumpingCfg.ChomskyNormalForm
+
+universe uT uN
+variable {T : Type uT}
+
+lemma ChomskyNormalFormRule.Rewrites.word {N : Type uN} {r : ChomskyNormalFormRule T N}
+    {u : List T} {v : List (Symbol T N)}
+    (hruv : r.Rewrites (u.map Symbol.terminal) v) :
+    False := by
+  induction u generalizing v with
+  | nil => cases hruv
+  | cons _ _ ih =>
+    cases hruv with
+    | cons _ _ hru => exact ih hru
+
+lemma ChomskyNormalFormGrammar.Produces.input_output
+    {g : ChomskyNormalFormGrammar.{uN} T} {r : ChomskyNormalFormRule T g.NT}
+    (hrg : r ∈ g.rules) :
+    g.Produces [.nonterminal r.input] r.output :=
+  ⟨r, hrg, ChomskyNormalFormRule.Rewrites.input_output⟩
