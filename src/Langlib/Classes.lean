e1856b365f0222c58c640ab41f588828572f2302
niels <niels.muendler@inf.ethz.ch>
2026-06-17 10:29:46 +0200

    Prove recursive closure under intersection

--- src/Langlib/Classes.lean
+++ src/Langlib/Classes.lean
@@ -131,6 +131,7 @@ public import Langlib.Classes.Linear.Pumping.Spine
 public import Langlib.Classes.Recursive.Basics.Elementary
 public import Langlib.Classes.Recursive.Basics.Post
 public import Langlib.Classes.Recursive.Closure.Complement
+public import Langlib.Classes.Recursive.Closure.Intersection
 public import Langlib.Classes.Recursive.Closure.Reverse
 public import Langlib.Classes.Recursive.Closure.Union
 public import Langlib.Classes.Recursive.Decidability.Membership
